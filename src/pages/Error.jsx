0dc78efe7e485d78951dfb45ddc5410e54d85e44
grashik99 <grashik99@gmail.com>
2025-06-04 20:12:35 +0300

    background complated

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router";
+import errorImage from "../assets/404.jpg";
+
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
-export default Error
+    <div
+      style={{ backgroundImage: `url(${errorImage})` }}
+      className=" min-h-screen w-full bg-cover bg-center flex items-center md:items-end-safe justify-center"
+    >
+      <Link
+        to="/"
+        className="text-4xl font-semibold bg-black/30 p-4 shadow-xl rounded-2xl border md:mb-30 text-white"
+      >
+        Page Not Found! Go To HomePage
+      </Link>
+    </div>
+  );
+};
+
+export default Error;
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/shared/Navbar";
 import Footer from "../components/shared/Footer";
+import bg from "../assets/bg.jpg";
 
 const Root = () => {
   return (
-    <div className="max-w-7xl mx-auto">
-      <Navbar />
-      <Outlet />
-      <Footer />
+    <div  style={{backgroundImage: `url(${bg})`}} className="min-h-screen bg-cover bg-center">
+      <div className="max-w-7xl mx-auto ">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
     </div>
   );
 };
